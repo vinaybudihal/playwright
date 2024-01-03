b81bf815b0c77779d3f8b997a2697ff6215d76a8
vinaybudihal <131458526+vinaybudihal@users.noreply.github.com>
2024-01-04 01:15:36 +0530

    Update test1.spec.js

--- playwright/tests/test1.spec.js
+++ playwright/tests/test1.spec.js
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  console.log("sample text: inside the test1");
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
