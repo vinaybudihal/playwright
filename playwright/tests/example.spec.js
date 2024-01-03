29b743ca4093401633049d0e331e1bcd98879459
vinaybudihal <131458526+vinaybudihal@users.noreply.github.com>
2024-01-04 04:18:50 +0530

    Update example.spec.js

--- playwright/tests/example.spec.js
+++ playwright/tests/example.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost');
+  await page.goto('https://127.0.0.1/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
